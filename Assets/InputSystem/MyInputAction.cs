71bc7c76d74c149f7fba2528150e9d900c7c0bb5
ShGuan0823 <38657307+ShGuan0823@users.noreply.github.com>
2021-11-25 01:22:49 +0000

    update input system

---
+++ Assets/InputSystem/MyInputAction.cs
@@ -0,0 +1,220 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSystem/MyInputAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @MyInputAction : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @MyInputAction()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""MyInputAction"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""ceee9155-df6c-4629-8e26-f3b449ab8fb9"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""28fd9b35-eb11-4a9e-97dc-ca26bac241b6"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""0b2ec6a8-a513-43d1-b1cb-330e171a7477"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""021a46e8-19a8-42ac-90cf-a6dfcbec88d9"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""787015fc-48ca-4ea8-a1e3-5f118c738e33"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e9b39c95-30ce-4764-a1de-9f275d7b7a4e"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""04744106-921b-436e-83cd-35b5c95c6d0e"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""329e2e6d-9dcf-43ba-8cfa-aa0a24919832"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""96301a19-6ac2-4397-a3f7-08c64f3724cf"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""077121a6-8399-4433-b8a5-e022f3c4f3a0"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""7c4eb361-addf-4cf9-b38e-18df30f4bd52"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_Movement = m_GamePlay.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_Movement;
+    public struct GamePlayActions
+    {
+        private @MyInputAction m_Wrapper;
+        public GamePlayActions(@MyInputAction wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_GamePlay_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    public interface IGamePlayActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -7,111 +7,169 @@ using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
-    private NavMeshAgent agent;
-    private Animator anim;
-    private CharacterStatus characterStatus;
-    private GameObject attackTarget;
-    private float lastAttackTime;
+    //private NavMeshAgent agent;
+    //private Animator anim;
+    //private CharacterStatus characterStatus;
+    //private GameObject attackTarget;
+    //private float lastAttackTime;
+
+    //private void Awake()
+    //{
+    //    agent = GetComponent<NavMeshAgent>();
+    //    anim = GetComponent<Animator>();
+    //    characterStatus = GetComponent<CharacterStatus>();
+    //}
+
+    //private void Start()
+    //{
+    //    MouseManager.instance.onMouseClicked += MoveToTargetByMouse;
+    //    MouseManager.instance.onEnemyClicked += EventAttack;
+    //}
+
+    //private void Update()
+    //{
+    //    //MoveByKeyBoard();
+    //    SwitchAnimation();
+    //    lastAttackTime -= Time.deltaTime;
+    //}
+
+    //void SwitchAnimation()
+    //{
+    //    anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    //}
+
+    //public void MoveToTargetByMouse(Vector3 target)
+    //{
+    //    StopAllCoroutines();
+    //    agent.isStopped = false;
+    //    agent.destination = target;
+    //}
+
+    //public void MoveByKeyBoard()
+    //{
+    //    StopAllCoroutines();
+    //    agent.isStopped = false;
+    //    agent.enabled = false;
+    //    // move value
+    //    float xm = 0, ym = 0, zm = 0;
+
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        zm += Time.deltaTime * characterStatus.characterData.currentSpeed;
+    //    }
+    //    else if (Input.GetKey(KeyCode.A))
+    //    {
+    //        xm -= -1 * Time.deltaTime * characterStatus.characterData.currentSpeed;
+    //    }
+    //    else if (Input.GetKey(KeyCode.S))
+    //    {
+    //        zm -= -1 * Time.deltaTime * characterStatus.characterData.currentSpeed;
+    //    }
+    //    else if (Input.GetKey(KeyCode.D))
+    //    {
+    //        xm += Time.deltaTime * characterStatus.characterData.currentSpeed;
+    //    }
+
+    //    transform.position = new Vector3(xm, ym, zm);
+    //    agent.enabled = true;
+    //}
+
+    //private void EventAttack(GameObject target)
+    //{
+    //    if (target != null)
+    //    {
+    //        attackTarget = target;
+    //        characterStatus.isCritical = Random.value < characterStatus.attackData.criticalChance;
+    //        StartCoroutine(MoveToAttackTarget());
+    //    }
+    //}
+
+    //IEnumerator MoveToAttackTarget()
+    //{
+    //    agent.isStopped = false;
+    //    transform.LookAt(attackTarget.transform);
+
+    //    while (Vector3.Distance(attackTarget.transform.position, transform.position) > characterStatus.attackData.attackRange)
+    //    {
+    //        agent.destination = attackTarget.transform.position;
+    //        yield return null;
+    //    }
+
+    //    agent.isStopped = true;
+    //    // Attack
+
+    //    if (lastAttackTime < 0)
+    //    {
+    //        anim.SetBool("Critical", characterStatus.isCritical);
+    //        anim.SetTrigger("Attack");
+    //        // 重置攻击冷却时间
+    //        lastAttackTime = characterStatus.attackData.coolDown;
+    //    }
+    //}
+
+    //// Animation Event
+    //void Hit()
+    //{
+    //    var targetStatus = attackTarget.GetComponent<CharacterStatus>();
+
+    //    targetStatus.TakeDamage(characterStatus, targetStatus);
+    //}
+
+    // TODO: change data sources
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    Rigidbody rigid;
+    MyInputAction inputActions;
+    Vector2 move;
+
+    private float forwardAmount;
+    private float rotateAmount;
 
     private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
-        characterStatus = GetComponent<CharacterStatus>();
+        rigid = GetComponent<Rigidbody>();
+        inputActions = new MyInputAction();
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        MouseManager.instance.onMouseClicked += MoveToTargetByMouse;
-        MouseManager.instance.onEnemyClicked += EventAttack;
+        inputActions.Enable();
+        inputActions.GamePlay.Movement.performed += Movement_performed;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+        inputActions.GamePlay.Movement.performed -= Movement_performed;
     }
 
     private void Update()
     {
-        //MoveByKeyBoard();
-        SwitchAnimation();
-        lastAttackTime -= Time.deltaTime;
+        onMove(move);
     }
 
-    void SwitchAnimation()
+    private void FixedUpdate()
     {
-        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
+        rigid.velocity = forwardAmount * transform.forward * moveSpeed;
+        rigid.MoveRotation(rigid.rotation * Quaternion.Euler(0, rotateAmount * rotateSpeed, 0));
     }
 
-    public void MoveToTargetByMouse(Vector3 target)
+
+    public void Movement_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        StopAllCoroutines();
-        agent.isStopped = false;
-        agent.destination = target;
+        move = obj.ReadValue<Vector2>();
     }
 
-    public void MoveByKeyBoard()
+
+    private void onMove(Vector2 direction)
     {
-        StopAllCoroutines();
-        agent.isStopped = false;
-        agent.enabled = false;
-        // move value
-        float xm = 0, ym = 0, zm = 0;
+        Vector3 wordlMove = new Vector3(direction.x, 0, direction.y);
+        Vector3 localMove = transform.InverseTransformVector(wordlMove);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            zm += Time.deltaTime * characterStatus.characterData.currentSpeed;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            xm -= -1 * Time.deltaTime * characterStatus.characterData.currentSpeed;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            zm -= -1 * Time.deltaTime * characterStatus.characterData.currentSpeed;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            xm += Time.deltaTime * characterStatus.characterData.currentSpeed;
-        }
-
-        transform.position = new Vector3(xm, ym, zm);
-        agent.enabled = true;
+        forwardAmount = localMove.z;
+        rotateAmount = Mathf.Atan2(localMove.x, localMove.z);
     }
 
-    private void EventAttack(GameObject target)
-    {
-        if (target != null)
-        {
-            attackTarget = target;
-            characterStatus.isCritical = Random.value < characterStatus.attackData.criticalChance;
-            StartCoroutine(MoveToAttackTarget());
-        }
-    }
 
-    IEnumerator MoveToAttackTarget()
-    {
-        agent.isStopped = false;
-        transform.LookAt(attackTarget.transform);
-
-        while (Vector3.Distance(attackTarget.transform.position, transform.position) > characterStatus.attackData.attackRange)
-        {
-            agent.destination = attackTarget.transform.position;
-            yield return null;
-        }
-
-        agent.isStopped = true;
-        // Attack
-
-        if (lastAttackTime < 0)
-        {
-            anim.SetBool("Critical", characterStatus.isCritical);
-            anim.SetTrigger("Attack");
-            // 重置攻击冷却时间
-            lastAttackTime = characterStatus.attackData.coolDown;
-        }
-    }
-
-    // Animation Event
-    void Hit()
-    {
-        var targetStatus = attackTarget.GetComponent<CharacterStatus>();
-
-        targetStatus.TakeDamage(characterStatus, targetStatus);
-    }
 }
